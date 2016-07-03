ac9c2002dd4ed0ca2256e61fb69152f8f337a379
guofangbi <bishion@163.com>
2016-07-03 13:17:16 +0800

    界面bug修改。

---
+++ src/main/java/com/bizi/blog/Application.java
@@ -0,0 +1,15 @@
+package com.bizi.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述：
+ * Created by GuoFangBi on 16-7-3.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
